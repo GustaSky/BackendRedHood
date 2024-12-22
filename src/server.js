8384649e2f895354460c9a20e32d8a779d258a21
Gusta <gustavowalker102@gmail.com>
2024-12-21 23:54:35 -0300

    fix: Configuração CORS

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 
 const app = express();
 
-// Middlewares
+// Configuração do CORS
 app.use(cors({
-  origin: 'https://gustasky.github.io',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+    origin: ['https://gustasky.github.io', 'http://localhost:5500'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
+
 app.use(express.json());
 
 // Rotas
@@ -18,10 +19,10 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/admin', require('./routes/admin'));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'API funcionando!' });
+    res.json({ message: 'API funcionando!' });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 }); 
