4203978c3647404857e65804e30b89dab2659ebe
Gusta <gustavowalker102@gmail.com>
2024-12-22 00:20:46 -0300

    fix: Atualiza configuração CORS

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 const app = express();
 
-// Configuração do CORS para desenvolvimento local
+// Configuração do CORS
 app.use(cors({
-    origin: '*', // Permite todas as origens durante testes
+    origin: '*', // Permite todas as origens durante desenvolvimento
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
