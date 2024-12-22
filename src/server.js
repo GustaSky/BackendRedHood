08f9f65a09516a70b7514a49e041b67e018596ec
Gusta <gustavowalker102@gmail.com>
2024-12-22 00:56:56 -0300

    acho q hoje nao durmo

--- src/server.js
+++ src/server.js
@@ -4,19 +4,37 @@ require('dotenv').config();
 
 const app = express();
 
-// Configuração do CORS - ANTES de todas as rotas
-app.use(cors({
-    origin: [
-        'https://gustasky.github.io',
-        'http://127.0.0.1:5500',
-        'http://localhost:5500'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false
-}));
+// Configuração mais permissiva do CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Responde a requisições OPTIONS
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
+    next();
+});
 
 app.use(express.json());
+
+// Log de requisições
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`, req.body);
+    next();
+});
+
+// Tratamento de erros
+app.use((err, req, res, next) => {
+    console.error('Erro:', err);
+    res.status(500).json({
+        error: 'server_error',
+        message: 'Erro interno do servidor',
+        details: err.message
+    });
+});
 
 // Rotas
 app.use('/api/users', require('./routes/users'));
